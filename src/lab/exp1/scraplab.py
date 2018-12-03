cf80c29ae94bcf233fe8dae9a7ee3ebf3d6dadd5
Khwaja-Bilkhis <44537677+Khwaja-Bilkhis@users.noreply.github.com>
2018-12-03 22:45:32 +0530

    Trailing white spaces

--- src/lab/exp1/scraplab.py
+++ src/lab/exp1/scraplab.py
@@ -5,17 +5,14 @@ def writefile(fname,s,s1,heading):
 	f=open(fname, 'w+')
 	f.write(template)
 	f.seek(0)
-	
 	content = f.read()
 	content=content.replace('Disciplines and Domains',breadcrumb)
 	f.seek(0)
 	f.write(content)
 	print content
-	
 	f.seek(0)
 	content=f.read()
 	k=content.index('<div class="col-md-10 lab-list-col-10">')
-	
 	t1=content.index('<!--edit1-->')
 	print t1
 	f.seek(t1+13)
@@ -42,7 +39,6 @@ d=['introduction','theory','objective','experiment','manual','quizzes','further_
 print len(sectionno)
 sectionNumber=1
 st=""
-
 att = ''+'experiment-article-heading'
 tagger = soup.findAll('header', attrs={'id':att,'class':'heading'})
 heading=str(tagger[0].text)
@@ -65,7 +61,7 @@ while sectionNumber<=len(sectionno):
 	tag1=tag1.strip()
 	print tag1
 	if tag1=='Prerequisite S/W':
-		tag1='Prerequisites'
+	tag1='Prerequisites'
 	st+='<a href="'+tag1+'.html?domain=Computer Science&lab='+heading1+'" class="sidebar-a" > <h3 class="text-h3-darkblue" style="margin-top: 2px;">'+tag1+'</h3></a>'	
 	sectionNumber=sectionNumber+1
 st+='<a href="Feedback.html?domain=Computer Science&lab='+heading1+'" class="sidebar-a" > <h3 class="text-h3-darkblue" style="margin-top: 2px;">Feedback</h3></a>'
@@ -81,14 +77,13 @@ while sectionNumber<=len(sectionno):
 	tag1=str(tagger[0].text)	
 	tag1=tag1.strip()
 	if tag1=='Prerequisite S/W':
-		tag1='Prerequisites'
+	tag1='Prerequisites'
 	att = ''+'experiment-article-section-'+str(sectionNumber)+'-content'
 	tagger = soup.findAll('div', attrs={'id':att,'class':'content'})
 	
 	tag+=str(tagger[0])
 	#print tag
 	#print st
-	
 	
 	writefile(tag1+'.html',tag,st,heading)
 	#print sectionNumber
