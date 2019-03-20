fa5cc450055a89fbc139df87cbad00647040b357
risubaba <risubhjain1010@gmail.com>
2019-03-20 16:59:25 +0530

    Fixed #241 #242

--- src/lab/final-build/EXP_1sep2010/exp5/preQuiz5.php
+++ src/lab/final-build/EXP_1sep2010/exp5/preQuiz5.php
@@ -229,6 +229,63 @@
     <!-- Include js plugin -->
   <script src="../../../vendors/owl-carousel/owl.carousel.js"></script>
   
+  <script>
+    //highlight color of answer - can change this color to a hex code or recognized color name
+    var highlightColor = "#0066cc";
+
+
+    //this should not be changed
+    function checkQuestionRadio(radioGroup) {
+
+    //go through the radio group sent in and determine if radio button
+    //checked is "correct".
+    //return 1 for correct value, 0 for incorrect
+
+       for (i=0; i<radioGroup.length; i++) {
+         if (radioGroup[i].checked) {
+           if (radioGroup[i].value == "correct") {
+             return 1;
+           }
+           else {
+             return 0;
+           }
+         }
+       }
+    return 0;
+    }
+
+
+    //this should not be changed
+    function highlightCorrectButton(radioButton) {
+       document.getElementById(radioButton).style.backgroundColor = highlightColor;
+    }
+
+    function checkQuiz() {
+       numCorrect = 0;
+       numCorrect += checkQuestionRadio( document.quiz.q1);
+       numCorrect += checkQuestionRadio( document.quiz.q2);
+       numCorrect += checkQuestionRadio( document.quiz.q3);
+       numCorrect += checkQuestionRadio( document.quiz.q4);
+       numCorrect += checkQuestionRadio( document.quiz.q5);
+     
+
+      //highlight correct answers from radio button groups...use span id name
+       highlightCorrectButton("correct1");
+       highlightCorrectButton("correct2");
+       highlightCorrectButton("correct3");
+       highlightCorrectButton("correct4");
+       highlightCorrectButton("correct5");
+
+
+       //produce output in textarea.
+       document.quiz.output.value =
+         "You got " + numCorrect + " out of 5 questions correct.\n" +
+         "The correct answers are highlighted." 
+
+    }
+  </script>
+
+
   <script type="text/javascript">
   $(document).ready(function() {
      
