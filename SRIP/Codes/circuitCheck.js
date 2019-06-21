e930536013c692a3cdd5629ec581faf587cb150c
Utkarsh Chhapekar <utkarshchhapekar2@gmail.com>
2019-06-21 16:29:48 +0530

    Made some changes in code

--- SRIP/Codes/circuitCheck.js
+++ SRIP/Codes/circuitCheck.js
@@ -1,6 +1,9 @@
 function circCheck()
 {
-	document.getElementById("sim").dispatchEvent(new MouseEvent("click", {ctrlKey: true}));
-	console.log(dataFinal.width);
 	console.log(JSON.stringify(dataFinal.devices));
+	console.log(JSON.stringify(dataFinal.connectors));
+	var dev = dataFinal.devices;
+	var con = dataFinal.connectors;
+	console.log(dev.length);
+	console.log(con.length);
 }
