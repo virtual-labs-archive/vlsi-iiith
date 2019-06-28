f08d6896ba7b4f3738c2742ace073ba7536754ae
Utkarsh Chhapekar <utkarshchhapekar2@gmail.com>
2019-06-28 17:50:08 +0530

    Finished project doc and finished the circuitcheck code

--- SRIP/Codes/Readme.txt
+++ SRIP/Codes/Readme.txt
@@ -7,6 +7,7 @@ VLSI | issue no 381 | Design of Pass transistor and Multiplexer
 Simulator:
 The simulator can be accessed by clicking on the simLayout.html file.
 Test cases can be seen in the Test_Cases folder.
+The Project documentation is in the project_doc.html file.
 
 Files used by the simulator:
 simLayout.html
--- SRIP/Codes/circuitCheck.js
+++ SRIP/Codes/circuitCheck.js
@@ -1,59 +1,167 @@
 function circCheck()
 {
-	console.log(JSON.stringify(dataFinal.devices));
-	console.log(JSON.stringify(dataFinal.connectors));
+	//console.log(JSON.stringify(dataFinal.devices));
+	//console.log(JSON.stringify(dataFinal.connectors));
 	var dev = dataFinal.devices;
 	var con = dataFinal.connectors;
-	console.log(dev.length);
-	console.log(con.length);
-	console.log(document.getElementById("whichSim").value);
+	//console.log(dev.length);
+	//console.log(con.length);
+	//console.log(document.getElementById("whichSim").value);
 	var opt = document.getElementById("whichSim").value;
+	var devic = new Object();
+	var connec = new Object();
+	connec = {};
+	devic = {};
+	//Main parsing done here:
+			for(var i = 0;i<dev.length;i++)
+			{
+				var a = JSON.stringify(dev[i].id);
+				var res = a.substring(1,a.length-1);
+				devic[res] = dev[i].type;
+			}
+			for(var i=0;i<con.length;i++)
+			{
+				var fro = JSON.stringify(con[i].from);
+				var too = JSON.stringify(con[i].to);
+				spl1 = "";
+				spl2 = "";
+				spl4 = "";
+				spl5 = "";
+				var flag = true;
+				for(var j=0;j<fro.length;j++)
+				{
+					if(fro[j]=="\"")
+					{
+						continue;
+					}
+					else
+					{
+						if(fro[j]==".")
+						{
+							flag=false;
+						}
+						if(flag)
+						{
+							spl1 = spl1 + fro[j];
+							spl4 = spl4 + too[j];
+						}
+						else
+						{
+							spl2 = spl2 + fro[j];
+							spl5 = spl5 + too[j];
+						}
+					}
+				}
+				var spl3 = "";
+				var spl6 = "";
+				spl3 = devic[spl1] + spl2;
+				spl6 = devic[spl4] + spl5;
+				connec[spl3] = spl6;
+			}
+	console.log(devic);
+	console.log(connec);
+	//-----------------------------------------------------------------------------
+
 	if(opt==="1")
 	{
-		if(dev.length==7 && con.length==7)
+		var check = {
+					"Delay.in0": "OSC.out", 
+					"In.in0": "NMOS.out", 
+					"NMOS.in0": "OSC.out",
+					"NMOS.in1": "Out.out", 
+					"Out.in0": "PMOS.out", 
+					"PMOS.in0": "Delay.out",
+					"PMOS.in1": "In.out"
+				};
+		if(isEqual(check, connec))
 		{
 			document.getElementById("blnk").style.display = "none";
 			document.getElementById("neg").style.display = "none";
 			document.getElementById("mux").style.display = "none";
 			document.getElementById("pos").style.display = "inline";
+			alert("Simulation successful!");
 		}
 		else
 		{
-			alert("This is not the correct circuit");
+			alert("This is not the correct circuit. Please refer procedure carefully");
+			return;
 		}
 
 	}
 	else if(opt==="2")
 	{
-		if(dev.length==7 && con.length==7)
+		var check = {
+					"Delay.in0": "OSC.out",
+					"In.in0": "NMOS.out",
+					"NMOS.in0": "Delay.out",
+					"NMOS.in1": "Out.out",
+					"Out.in0": "PMOS.out",
+					"PMOS.in0": "OSC.out",
+					"PMOS.in1": "In.out"
+					};
+		if(isEqual(check, connec))
 		{
 			document.getElementById("blnk").style.display = "none";
 			document.getElementById("neg").style.display = "inline";
 			document.getElementById("mux").style.display = "none";
 			document.getElementById("pos").style.display = "none";
+			alert("Simulation successful!");
 		}
 		else
 		{
-			alert("This is not the correct circuit");
+			alert("This is not the correct circuit. Please refer procedure carefully");
+			return;
 		}
 	}
 	else if(opt==="3")
 	{
-		if(dev.length==9 && con.length==10)
+		var check = {
+					"Delay.in0": "OSC.out",
+					"OR.in0": "PassTransistor.out",
+					"OR.in1": "PassTransistor.out",
+					"Out.in0": "OR.out",
+					"PassTransistor.in0": "Delay.out",
+					"PassTransistor.in1": "In.out",
+					"PassTransistor.in2": "OSC.out"
+		}
+		if(isEqual(check, connec))
 		{
 			document.getElementById("blnk").style.display = "none";
 			document.getElementById("neg").style.display = "none";
 			document.getElementById("mux").style.display = "inline";
 			document.getElementById("pos").style.display = "none";
+			alert("Simulation successful!");
 		}
 		else
 		{
-			alert("This is not the correct circuit");
+			alert("This is not the correct circuit. Please refer procedure carefully");
+			return;
 		}
 
 	}
 	else
 	{
 		alert("Please select the type of simulation");
+		return;
 	}
 }
+
+function isEqual(objA, objB)
+{
+	
+	var aProps = Object.getOwnPropertyNames(objA);     
+	var bProps = Object.getOwnPropertyNames(objB);
+	
+	
+	if (aProps.length != bProps.length) {         
+	    return false;     
+	}      
+	for (var i = 0; i < aProps.length; i++) {         
+	     
+	     var propName = aProps[i];            
+	     if (objA[propName] !== objB[propName]) {             
+	         return false;         
+	     }     
+	}
+	return true; 
+}  
