155ce53db3233138e17d1b6f15ddd37f4cbf090b
mrudhvika940 <mrudhvikadamaraju@gmail.com>
2020-04-01 00:03:25 +0530

    added header and footer for all experiments

--- src/lab/final-build/blank.php
+++ src/lab/final-build/blank.php
@@ -3,6 +3,8 @@
 <!DOCTYPE HTML>
 <html>
 <head>
+<script class='gtm'>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-W59SWTR');</script>
+
 <title>Virtual Labs - IIT Bombay</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <style type="text/css">
